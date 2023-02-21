f0333b36e10170b03b6abca19b5b2d77fb4f9770
SenpaiVR <SenpaiShichi@gmail.com>
2023-02-20 20:18:34 -0500

    update router to have auto redirects based on authentication

--- dist/assets/Dashboard.9d563ce5.js
+++
@@ -1,1 +0,0 @@
-import{d as s,u as o,s as t,c as a,o as c,a as n}from"./index.f2c7ee66.js";const r={class:"home"},_=n("h1",null,"HOME",-1),d=[_],p=s({__name:"Dashboard",setup(h){const e=o();return t(e),(i,l)=>(c(),a("div",r,d))}});export{p as default};
