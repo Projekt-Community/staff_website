b646792dc3dde72e61593611f5db18fdec6b25c0
SenpaiVR <SenpaiShichi@gmail.com>
2023-02-21 11:29:52 -0500

    Update login checker

---
+++ dist/assets/Dashboard.594d60f6.js
@@ -0,0 +1,1 @@
+import{d as s,u as o,s as t,c as a,o as c,a as n}from"./index.52b71f3b.js";const r={class:"home"},_=n("h1",null,"HOME",-1),d=[_],p=s({__name:"Dashboard",setup(h){const e=o();return t(e),(i,l)=>(c(),a("div",r,d))}});export{p as default};
